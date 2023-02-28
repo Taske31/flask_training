ba4c5c4fe8cd5c50c3dd8f278266d13722deb2e4
Aleksey <alekseik2017@mail.ru>
2023-02-28 23:06:37 +0300

    Первая работа

--- data/jobs.py
+++ data/jobs.py
@@ -10,7 +10,7 @@ class Jobs(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("users.id"))
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     job = sqlalchemy.Column(sqlalchemy.Text)
     work_size = sqlalchemy.Column(sqlalchemy.Integer)
     collaborators = sqlalchemy.Column(sqlalchemy.Text)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -50,6 +51,15 @@ def main():
     user.address = "module_4"
     user.email = "h2o@mars.org"
     db_sess.add(user)
+
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
+    db_sess.add(job)
     db_sess.commit()
 
 
